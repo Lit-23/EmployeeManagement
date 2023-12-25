500065a4c8084dc24a4014e8e578942c9326165b
Lit-23 <rommelmarquez143@gmail.com>
2023-12-25 09:47:00 -0800

    added redux persist

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import employeeReducer from './employeeSlice/employeeSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({employee: employeeReducer});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
-    employee: employeeReducer,
+    employee: persistedReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
+
+export const persistor = persistStore(store);
