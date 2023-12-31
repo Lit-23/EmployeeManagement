6999e03995057eaecd761a8d1b2fa0b5b2d0e44d
Lit-23 <rommelmarquez143@gmail.com>
2023-12-30 23:51:32 -0800

    done with dashboard. but i still have 1 error in rendering the avgsalary data. will fix later

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -8,42 +8,15 @@ export default function DashboardCard({ title, icon, value, link }) {
     navigate(link);
   };
 
-  // Employees data from API
-  // const [collection, setCollection] = useState({});
-  const [totalEmployee, setTotalEmployee] = useState(0);
-  // const [salary, setSalary] = useState([]);
-  const salaryArr = [];
-
-  useEffect(() => {
-    const searchEmployee = async () => {
-      try {
-        const res = await fetch('/api/employee/list', { method: 'GET' });
-        const data = await res.json();
-        // setCollection(data);
-        setTotalEmployee(data.length);
-        
-        // data.map((employee)=>(
-        //   setSalary([
-        //     ...salary, employee.salary
-        //   ])
-        // ))
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    searchEmployee();
-  }, [])
-
-  // console.log(salary);
   return (
     <div onClick={handleClick} className='flex flex-col flex-1 rounded-lg border-[1px] border-green-600 p-5 cursor-pointer'>
       <div className='flex justify-between items-center mb-2'>
         <h3 className='text-xl'>{title}</h3>
         <img src={icon} alt={title} />
       </div>
-      <div className='text-2xl font-[300] text-slate-600'>{`${value || totalEmployee || '$'+salaryArr}`}</div>
+      <div className='text-2xl font-[300] text-slate-600'>
+        {value}
+      </div>
     </div>
   )
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,39 @@
 import { useState, useEffect } from 'react';
-import ongoingProjects from '../constants/ongoingProjects.json';
-import completedProjects from '../constants/completedProjects.json';
 import DashboardCard from '../components/DashboardCard';
 import { ongoingIcon, completedIcon, salaryIcon, employeeIcon } from '../assets/icons';
 import TenureBarChart from '../components/TenureBarChart';
 import DesignationLineChart from '../components/DesignationLineChart';
 
 export default function Dashboard() {
-  // completed project
-  // ongoing project
-  // avg. salary
-  // employee
+  const [collection, setCollection] = useState([]);
+  const [totalEmployee, setTotalEmployee] = useState(0);
+  const [totalSalary, setTotalSalary] = useState(0);
+
+  useEffect(() => {
+    const searchEmployee = async () => {
+      try {
+        const res = await fetch('/api/employee/list', { method: 'GET' });
+        const data = await res.json();
+        setCollection(data);
+        setTotalEmployee(data.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    searchEmployee();
+
+    const searchSalary = async () => {
+      try { 
+        collection.map((employee) => setTotalSalary(prev => (prev + Number(employee.salary))))
+        console.log(totalSalary);
+  
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    searchSalary();
+  }, [])
 
   return (
     <section>
@@ -18,8 +41,8 @@ export default function Dashboard() {
       <div className='flex max-[769px]:flex-col justify-between gap-3 mb-5'>
         <DashboardCard title={'Ongoing Projects'} icon={ongoingIcon} value={5} link='/ongoing-projects'/>
         <DashboardCard title={'Completed Projects'} icon={completedIcon} value={20} link='/completed-projects'/>
-        <DashboardCard title={'Employees'} icon={employeeIcon} link='/employee-list'/>
-        <DashboardCard title={'Avg. Salary'} icon={salaryIcon} value={`$${500}`}/>
+        <DashboardCard title={'Employees'} icon={employeeIcon} value={totalEmployee} link='/employee-list'/>
+        <DashboardCard title={'Avg. Salary'} icon={salaryIcon} value={'$' + (totalSalary/totalEmployee).toFixed(0)}/>
       </div>
 
       <div className='flex max-[769px]:flex-col justify-center gap-10 max-[769px]:gap-5'>
