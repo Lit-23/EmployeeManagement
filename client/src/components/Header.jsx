dad605ae95fd7c74154740c8e1203655a5f66ff7
Lit-23 <rommelmarquez143@gmail.com>
2024-01-11 03:46:02 -0800

    for submission

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import { Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 export default function Header() {  
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <header className='px-5 py-[8px] flex items-center justify-between bg-white shadow-md fixed left-0 right-0 top-0'>
       <Link to='/'>
         <h1 className='text-xl font-[500] text-[#2e7d32]'>LitFlix</h1>
       </Link>
-      <div>
+      <div className="max-[769px]:hidden">
         <ul className='flex justify-center items-center'>
           <Link to='/'>
             <li 
@@ -22,7 +26,7 @@ export default function Header() {
               id="about" 
               className="hover:bg-slate-100 px-[6px] py-3 rounded-t-lg"
             >
-              About
+              Help
             </li>
           </Link>
           <Link to='/employee-login'>
@@ -43,6 +47,29 @@ export default function Header() {
           </Link>
         </ul>
       </div>
+      <div className="md:hidden py-2" onClick={e=>setOpen(true)}>
+        <MenuIcon sx={{color:['#2e7d32']}}/>
+      </div>
+
+      {/* modal */}
+      <Menu 
+        open={open} 
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        sx={{mt:-5}}
+      >
+        <MenuItem onClick={e=>{setOpen(false); navigate('/');}}>Home</MenuItem>
+        <MenuItem onClick={e=>{setOpen(false); navigate('/about');}}>Help</MenuItem>
+        <MenuItem onClick={e=>{setOpen(false); navigate('/employee-login');}}>Employee Login</MenuItem>
+        <MenuItem onClick={e=>{setOpen(false); navigate('/admin-login');}}>Admin Login</MenuItem>
+      </Menu>
     </header>
   )
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,11 +8,14 @@ export default function About() {
       <Header />
       <section className="text-xl max-[768px]:text-lg max-[640px]:text-base min-h-[85vh] p-5 mt-16 flex flex-col items-center">
         <h1 className="font-[500] text-3xl max-[640px]:text-2xl mb-3">
-          About Us
+          Instructions for the Employee Management Web App:
         </h1>
-        <p className='max-w-[800px] flex text-center'>
-          At Litflix, we are more than just a software development firm, we are your partners in turning your vision into reality. With a passion for innovation and a commitment to excellence, we offer flexible and customized software development services to meet the unique needs of your business.
-        </p>
+        <ol className='max-w-[800px] flex flex-col text-center list-decimal list-inside'>
+          <li className='mb-2'>Access the admin portal by logging in.</li>
+          <li className='mb-2'>Add new employees to the system.</li>
+          <li className='mb-2'>The employee's default login credentials for the employee portal will be their email and company ID.</li>
+          <li className='mb-2'>Update or delete employee data in the admin portal as needed.</li>
+        </ol>
       </section>
       <Footer />
     </>
