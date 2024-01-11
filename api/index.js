b021dd422cf7f001492d3721a4ff58ffa243cebd
Lit-23 <rommelmarquez143@gmail.com>
2024-01-11 02:30:59 -0800

    test deploy in render

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from 'path';
 dotenv.config(); 
 
 import employeeRoutes from "./routes/employee.route.js"
@@ -15,23 +15,9 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 const app = express();
-
-const whitelist = [
-  "https://employee-management-client-lyart.vercel.app/dashboard",
-  "https://employee-management-client-lyart.vercel.app/profile",
-  "https://employee-management-client-lyart.vercel.app/add-employee",
-  "https://employee-management-client-lyart.vercel.app/employee-list",
-  "https://employee-management-client-lyart.vercel.app/update"
-]
-
-app.use(cors(
-  {
-    origin: whitelist,
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true
-  }
-))
 
 app.listen(3000, () => {
   console.log('Server listening to port 3000!')
@@ -42,6 +28,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/employee', employeeRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
